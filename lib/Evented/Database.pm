e7bafb47e66d332db2d371956cf10f0f8198223c
Mitchell Cooper <githubspam@rlygd.net>
2014-08-04 17:17:29 -0400

    1.03: combine results of configuration and database

--- lib/Evented/Database.pm
+++ lib/Evented/Database.pm
@@ -15,7 +15,7 @@ use DBI qw(SQL_BLOB SQL_INTEGER SQL_FLOAT SQL_VARCHAR);
 use Evented::Database::Table;
 use Evented::Database::Rows;
 
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 our $json    = JSON::XS->new->allow_nonref(1);
 
 ###############################
@@ -38,6 +38,73 @@ sub new {
     return $db;
 }
 
+# returns true if either have the block.
+sub has_block {
+    my $db = shift;
+    $db->_has_block(@_) || $db->SUPER::has_block(@_);
+}
+
+# returns a simplified combination of block names (no duplicates).
+sub names_of_block {
+    my $db = shift;
+    my @names1 = $db->_names_of_block(@_);
+    my @names2 = $db->SUPER::names_of_block(@_);
+    my %h = map { $_ => 1 } @names2, @names1;
+    return keys %h;
+}
+
+# returns a simlified combination of block hashes.
+# no duplicates; database overrides configuration.
+sub hash_of_block {
+    my $db = shift;
+    my %hash1 = $db->_hash_of_block(@_);
+    my %hash2 = $db->SUPER::hash_of_block(@_);
+    my %hash3 = (%hash2, %hash1);
+    return %hash3;
+}
+
+# returns a simplified combination of block keys (no duplicates).
+sub keys_of_block {
+    my $db = shift;
+    my @keys1 = $db->_keys_of_block(@_);
+    my @keys2 = $db->SUPER::keys_of_block(@_);
+    my %h = map { $_ => 1 } @keys1, @keys2;
+    return keys %h;
+}
+
+# returns a simplified combination of block values.
+# if the same pair exists in both, the database overrides.
+sub values_of_block {
+    my $db = shift;
+    # TODO: this might require more sophistication.
+    return;
+}
+
+# get a configuration or simple database value.
+# this is a bit more sophisticated than the rest.
+sub get {
+    my @at_underscore = @_[1..$#_];
+    my ($db, $b_type, $b_name, $key) = &_args;
+    my $row = $db->table('configuration')->row(
+        blocktype => $b_type,
+        block     => $b_name,
+        key       => $key
+    );
+        
+    # no match; forward.
+    # we check count here in case the value is undef or NULL
+    # in the database (in which case it should still override).
+    return $db->SUPER::get(@at_underscore) unless $row->count;
+    
+    my $value = $row->select('value');
+    return $value unless length $value;
+    return $json->decode($value);
+}
+
+##################################################
+### Database versions of configuration methods ###
+##################################################
+
 # sub parse_config()
 # perhaps we should clear the cache here for the desired rehasing effect.
 
@@ -46,7 +113,7 @@ sub new {
 # e.g.  ->has_block('namelessblock')
 #       ->has_block(['named', 'block'])
 #
-sub has_block {
+sub _has_block {
     my ($db, $b_type, $b_name) = &_args;
     return $db->table('configuration')->rows(
         blocktype => $b_type,
@@ -59,7 +126,7 @@ sub has_block {
 # e.g.  ->names_of_block('listen')
 #       returns ('0.0.0.0', '127.0.0.1')
 #
-sub names_of_block {
+sub _names_of_block {
     my ($db, $b_type) = @_;
     my @names = $db->table('configuration')->rows(blocktype => $b_type)->select('block');
     my %h = map { $_ => 1 } @names;
@@ -68,7 +135,7 @@ sub names_of_block {
 }
 
 # returns the key:value hash (not ref) of a block.
-sub hash_of_block {
+sub _hash_of_block {
     my ($db, $b_type, $b_name) = &_args;
     my @rows = $db->table('configuration')->rows(
         blocktype => $b_type,
@@ -82,7 +149,7 @@ sub hash_of_block {
 # e.g.  ->keys_of_block('modules')
 #       returns an array of every module
 #
-sub keys_of_block {
+sub _keys_of_block {
     my ($db, $b_type, $b_name) = &_args;
     return $db->table('configuration')->rows(
         blocktype => $b_type,
@@ -91,25 +158,13 @@ sub keys_of_block {
 }
 
 # returns a list of all the values in a block.
-sub values_of_block {
+sub _values_of_block {
     my ($db, $b_type, $b_name) = &_args;
     my $value = $db->table('configuration')->rows(
         blocktype => $b_type,
         block     => $b_name
     )->select('value');
     return defined $value ? $json->decode($value) : $db->SUPER::values_of_block(@_);
-}
-
-# get a configuration or simple database value.
-sub get {
-    my ($db, $b_type, $b_name, $key) = &_args;
-    my $value = $db->table('configuration')->row(
-        blocktype => $b_type,
-        block     => $b_name,
-        key       => $key
-    )->select('value');
-    my $t = defined $value ? $json->decode($value) : $db->SUPER::get(@_);
-    return $t;
 }
 
 ############################
@@ -127,7 +182,7 @@ sub store {
         blocktype => $b_type,
         block     => $b_name,
         key       => $key
-    )->insert_or_update(value => $json->encode($value));
+    )->insert_or_update(value => encode($value));
 }
 
 # return a table object.
@@ -146,7 +201,7 @@ sub write_conf_to_db {
             blocktype => $b_type,
             block     => $b_name,
             key       => $key
-        )->insert_or_update(value => $json->encode($value));
+        )->insert_or_update(value => encode($value));
     }}}
 }
 
@@ -213,6 +268,12 @@ sub _args {
 sub _block {
     my $block = shift;
     return (ref $block && ref $block eq 'ARRAY' ? @$block : ('section', $block));
+}
+
+sub encode {
+    my $value = shift;
+    return $value unless defined $value;
+    return $json->encode($value);
 }
 
 sub EDB_STRING  { bless [SQL_VARCHAR, shift], 'Evented::Database::DataType' }
